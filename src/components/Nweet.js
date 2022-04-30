fe6ccc1578c7f919c4a966bf504025a9b623f651
xgudwnzoooox <baehju2718@naver.com>
2022-05-01 08:46:12 +0900

    delete nweet that has text and image

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { doc, deleteDoc, updateDoc }from "firebase/firestore";
+import { deleteObject, ref } from "@firebase/storage";
 
 const Nweet = ({ nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
@@ -12,6 +13,9 @@ const Nweet = ({ nweetObj, isOwner }) => {
     const ok = window.confirm("Are you sure you want to delete this nweet?");
     if (ok) {
       await deleteDoc(NweetTextRef );
+      if(nweetObj.attachmentUrl !== "" ){
+        await deleteObject(ref(storageService, nweetObj.attachmentUrl));
+      }
     }
   };
   const toggleEditing = () => setEditing((prev) => !prev);
