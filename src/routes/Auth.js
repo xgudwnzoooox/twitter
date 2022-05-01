f09a93e10f5070310255fb6ffbe51f4ad2519cdf
xgudwnzoooox <baehju2718@naver.com>
2022-05-02 05:23:44 +0900

    clean the code

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { authService } from "fbase";
+import AuthForm from "components/AuthForm";
 
 import {
   getAuth,
@@ -14,36 +15,7 @@ const auth = getAuth();
 
 
 const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(true);
-  const [error, setError] = useState("");
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      let data;
-      if (newAccount) {
-        data = await createUserWithEmailAndPassword(auth, email, password);
-      } else {
-      //   data = await authService.signInWithEmailAndPassword(email, password);
-      // }
-        data = await signInWithEmailAndPassword(auth, email, password);
-      console.log(data);}
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-  const toggleAccount = () => setNewAccount((prev) => !prev);
+  
   const onSocialClick = async (event) => {
     const name = event.target.name;
     let provider;
@@ -52,36 +24,11 @@ const Auth = () => {
     } else if (name === "github") {
     provider = new GithubAuthProvider();
     }
-    const data = await signInWithPopup(authService, provider);
+    await signInWithPopup(authService, provider);
   };
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={onChange}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={onChange}
-        />
-        <input
-          type="submit"
-          value={newAccount ? "Create Account" : "Sign In"}
-        />
-        {error}
-      </form>
-      <span onClick={toggleAccount}>
-        {newAccount ? "Sign In" : "Create Account"}
-      </span>
+      <AuthForm />
       <div>
         <button onClick={onSocialClick} name="google">
           Continue with Google
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,7 @@
 import Nweet from "components/Nweet";
+import NweetFactory from "components/NweetFactory";
 import { dbService, storageService } from "fbase";
+import React, { useEffect, useRef, useState } from "react";
 import {
   addDoc,
   collection,
@@ -7,43 +9,9 @@ import {
   query,
   orderBy,
   } from "firebase/firestore";
-import { ref, uploadString, getDownloadURL } from "@firebase/storage";
-import React, { useEffect, useRef, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const Home = ({userObj}) => {
-  const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
-  const fileInput = useRef();
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
-      //storage 참조 경로로 파일 업로드 하기
-      const uploadFile = await uploadString(fileRef, attachment, "data_url");
-      console.log(uploadFile);
-      //storage에 있는 파일 URL로 다운로드 받기
-      attachmentUrl = await getDownloadURL(uploadFile.ref);
-    }
-
-    const nweetObj = {
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId : userObj.uid,
-      attachmentUrl,
-    };
-    
-    await addDoc(collection(dbService, "nweets"), nweetObj);
-    setAttachment("");
-    setNweet("");
-  };
-
-  const onChange = ({ target: { value } }) => {
-    setNweet(value);
-  };
 
   useEffect(() => {
     const q = query(collection(dbService, "nweets"),
@@ -60,45 +28,9 @@ const Home = ({userObj}) => {
     });
   }, []);
 
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-
-  const onClearAttachment = (event) => {
-    setAttachment("");
-    fileInput.current.value = "";
-  }
-
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={nweet}
-          onChange={onChange}
-          type="text"
-          placeholder="What's on your mind?"
-          maxLength={120}
-        />
-        <input type="file" accept="image/*" onChange={onFileChange} ref = {fileInput} />
-        <input type="submit" value="Nweet" />
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" height="50px" />
-            <button onClick={onClearAttachment}>Clear</button>
-          </div>
-        )}
-      </form>
+      <NweetFactory userObj={userObj} />
       <div>
         {nweets.map((nweet) => (
           <Nweet
